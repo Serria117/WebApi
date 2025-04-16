6621ea160bc893fc7709350df4aaa2ae6460d028
Dth256 <june25th87@gmail.com>
2025-04-16 12:59:59 +0700

    update check hashed value for duplicated file

---
+++ WebApp/Services/DocumentService/Dto/DocumentDisplayDto.cs
@@ -0,0 +1,18 @@
+﻿using WebApp.Enums;
+
+namespace WebApp.Services.DocumentService.Dto;
+
+public class DocumentDisplayDto
+{
+    public int Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string? Name { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+    public DateTime UploadTime { get; set; }
+    public DocumentType DocumentType { get; set; }
+    public string? Period { get; set; } = string.Empty;
+    public int NumberOfAdjustment { get; set; } = 0;
+    public string? PeriodType { get; set; } = string.Empty;
+    public DateTime DocumentDate { get; set; }
+    public string? AdjustmentType { get; set; }
+}
