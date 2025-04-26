984e99560f8d256ba7e1b2e7c7d4e47926768929
Dth256 <june25th87@gmail.com>
2025-04-26 15:38:03 +0700

    check

--- WebApp.Enums/HubName.cs
+++ WebApp.Enums/HubName.cs
@@ -2,7 +2,8 @@
 
 public struct HubName
 {
-    public const string PurchaseInvoice = "RetrieveList";
+    public const string InvoiceMessage = "RetrieveList";
+    public const string SoldInvoice = "SoldInvoice";
     public const string InvoiceCount = "InvoiceCount";
     public const string TotalProgress = "TotalProgress";
     public const string CountProgress = "CountProgress";
--- WebApp.Mongo/DocumentModel/IInvoiceInterface.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebApp.Mongo.DocumentModel;
-
-public interface IInvoiceInterface
-{
-    
-}
