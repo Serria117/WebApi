cc851585ca3bf03f3110baca3255a4e18629297d
Dth256 <june25th87@gmail.com>
2025-04-26 15:38:33 +0700

    check2

--- WebApp/ScheduleTask/JobConfigurator.cs
+++ WebApp/ScheduleTask/JobConfigurator.cs
@@ -12,25 +12,23 @@ public static class JobConfigurator
     /// <returns>The updated <see cref="IServiceCollection"/> instance.</returns>
     public static IServiceCollection AddQuartzJobs(this IServiceCollection services)
     {
+        var vnTimeZone = TZConvert.GetTimeZoneInfo("Asia/Ho_Chi_Minh");
         services.AddQuartz(q =>
         {
-            // Múi giờ Việt Nam
-            var vnTimeZone = TZConvert.GetTimeZoneInfo("Asia/Ho_Chi_Minh");
-
             // Đăng ký job + trigger
-            var jobKey = new JobKey("ClearOldRefreshTokenJob");
-            
-            q.AddJob<ClearOldRefreshTokenJob>(opts => opts.WithIdentity(jobKey));
-
+            var clearTokenJob = new JobKey("ClearOldRefreshTokenJob");
+            q.AddJob<ClearOldRefreshTokenJob>(opts => opts.WithIdentity(clearTokenJob));
             q.AddTrigger(opts => opts
-                                 .ForJob(jobKey)
+                                 .ForJob(clearTokenJob)
                                  .WithIdentity("ClearOldRefreshTokenJob-trigger#1")
-                                 .WithCronSchedule(CronExp.EveryDayAt00, x => x.InTimeZone(vnTimeZone))  // 0h mỗi ngày
+                                 .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(0, 0).InTimeZone(vnTimeZone)) // Thực hiện hàng ngày lúc 0h0m
+                                 .StartNow() // Bắt đầu ngay lập tức khi ứng dụng khởi động
             );
             
         });
         // Đăng ký các job khác ở đây...
         
+
         // Thêm hosted service để chạy các job đã đăng ký
         services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
         return services;
