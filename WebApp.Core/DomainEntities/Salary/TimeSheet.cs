505c1955fa45c5954457e65a088ba7a512b0bff4
Dth256 <june25th87@gmail.com>
2025-05-20 11:21:26 +0700

    Payroll features

---
+++ WebApp.Core/DomainEntities/Salary/TimeSheet.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WebApp.Enums;
+
+namespace WebApp.Core.DomainEntities.Salary;
+
+public class TimeSheet : BaseEntityAuditable<long>
+{
+    [ForeignKey(nameof(PayrollRecordId))]
+    public required PayrollRecord PayrollRecord { get; set; }
+
+    public long PayrollRecordId { get; set; }
+    
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+
+    public bool IsWeekend { get; set; } = false;
+    public bool IsHoliday { get; set; } = false;
+
+    public Guid EmployeeId { get; set; }
+    public Guid OrganizationId { get; set; }
+
+    public LeaveType Leave { get; set; } = LeaveType.NoLeave;
+}
