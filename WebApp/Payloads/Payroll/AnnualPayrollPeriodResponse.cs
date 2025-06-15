f0a72489328bac9f87547afa41aba024f86b638e
Dth256 <june25th87@gmail.com>
2025-06-15 21:55:42 +0700

    keep removing payroll

--- WebApp/Payloads/Payroll/AnnualPayrollPeriodResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebApp.Services.PayrollService.Dto;
-
-namespace WebApp.Payloads.Payroll;
-
-public class AnnualPayrollPeriodResponse
-{
-    public int Year { get; set; }
-    public List<PayrollPeriodDisplayDto> PayrollPeriods { get; set; } = [];
-}
