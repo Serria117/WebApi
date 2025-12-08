2b700c4f6069646f6a343224c2017657690525c6
hadt2 <june25th87@gmail.com>
2025-12-08 18:38:02 +0700

    increase default item per page to 1000

--- WebApp/Payloads/PageRequest.cs
+++ WebApp/Payloads/PageRequest.cs
@@ -27,7 +27,7 @@ public class PageRequest
         return new PageRequest
         {
             Page = pr.Page ?? 1,
-            Size = pr.Size ?? 100,
+            Size = pr.Size ?? 1000,
             SortBy = pr.SortBy ?? "Id",
             OrderBy = pr.OrderBy ?? SortOrder.ASC,
             Sort = $"{pr.SortBy} {pr.OrderBy}",
